3f1fbaeaf734d60514b4ccddecaad0fa3935b098
Márcio DEV <playdznvlt@gmail.com>
2026-01-09 16:00:01 -0300

    feat: update version to 1.0.15 and refactor gitignore content generation for improved readability

--- src/commands/create/components/gitignore.ts
+++ src/commands/create/components/gitignore.ts
@@ -2,8 +2,15 @@ import { CreateProjectData } from '../types.js';
 
 export const getGitignoreContent = (projectData: CreateProjectData) => {
 	return (
-		['node_modules', 'dist', '', '# Environment', '.env', '.env.*', '!.env.example', '', '# Misc', '.npmrc'].join(
-			'\n'
-		) + '\n'
+		(
+			[
+				'node_modules',
+				'dist',
+				...(projectData.createDotEnv ? ['', '# Environment', '.env', '.env.*', '!.env.example'] : []),
+				'',
+				'# Misc',
+				'.npmrc'
+			] as string[]
+		).join('\n') + '\n'
 	);
 };
